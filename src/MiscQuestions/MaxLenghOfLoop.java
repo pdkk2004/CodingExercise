7ebcd4b7b5fa715e879f58bcedf8e1d848e4e9e1
pdkk2004 <kkdpan@gmail.com>
2015-10-24 22:44:04 -0700

    Add MaxLenghOfLoop.

---
+++ src/MiscQuestions/MaxLenghOfLoop.java
@@ -0,0 +1,35 @@
+package MiscQuestions;
+
+import org.junit.Assert;
+
+public class MaxLenghOfLoop {
+	
+	public static int maxLenghOfLoop(int[] array) {
+		boolean[] visited = new boolean[array.length];
+		int[] steps = new int[array.length];
+		int maxLoop = 0;
+		for (int i = 0; i < array.length && !visited[i]; i++) {
+			int index = i;
+			int step = 0;
+			while (index >= 0 && index < array.length) {
+				step++;
+				steps[index] = step;
+				visited[index] = true;
+				index = array[index];
+				if (visited[index]) {
+					maxLoop = Math.max(maxLoop, step - steps[index] + 1);
+					break;
+				}
+			}
+		}
+		return maxLoop;
+	}
+	
+	public static void main(String[] args) {
+		Assert.assertEquals(5, maxLenghOfLoop(new int[]{1, 2, 3, 4, 0}));
+		Assert.assertEquals(4, maxLenghOfLoop(new int[]{3, 5, 1, 6, 2, 4, 0}));
+		Assert.assertEquals(1, maxLenghOfLoop(new int[]{2, 2, 2, 2}));
+		Assert.assertEquals(3, maxLenghOfLoop(new int[]{3, 2, 1, 4, 0}));
+		Assert.assertEquals(3, maxLenghOfLoop(new int[]{1, 2, 3, 4, 2, 0}));
+	}
+}
