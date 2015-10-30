ca48adeefbca0c277530daae763a84959997bb4c
pdkk2004 <kkdpan@gmail.com>
2015-10-29 22:40:35 -0700

    Add All factors of product of distinct primes solution.

---
+++ src/MiscQuestions/AllFactorsProducts.java
@@ -0,0 +1,41 @@
+package MiscQuestions;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * http://www.fgdsb.com/2015/01/17/factors-of-product-of-distinct-primes/
+ * @author kkdpan
+ *
+ */
+public class AllFactorsProducts {
+
+	public static List<Integer> allFactorsProducts(int[] primes) {
+		List<Integer> ret = new LinkedList<>();
+		ret.add(1);
+		helper(primes, 0, 1, ret);
+		return ret;
+	}
+	
+	private static void helper(int[] primes, int index, int product, List<Integer> ret) {
+		if (index == primes.length) {
+			return;
+		}
+		
+		for (int i = index; i < primes.length; i++) {
+			product *= primes[i];
+			ret.add(product);
+			helper(primes, i + 1, product, ret);
+			product /= primes[i];
+		}
+	}
+	
+	@Test
+	public void test() {
+		int[] primes = {2, 3, 7};
+		List<Integer> ret = allFactorsProducts(primes);
+		System.out.println(ret.toString());
+	}
+}
