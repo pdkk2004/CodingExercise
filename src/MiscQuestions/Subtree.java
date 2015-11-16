116e33683e1594fe32581a8e7aec0b39bd99e9f1
pdkk2004 <kkdpan@gmail.com>
2015-11-15 23:38:03 -0800

    Add isSubtree problem solution. http://www.fgdsb.com/2015/01/03/subtree/

---
+++ src/MiscQuestions/Subtree.java
@@ -0,0 +1,38 @@
+package MiscQuestions;
+
+import java.util.Stack;
+
+import Common.TreeNode;
+
+/**
+ * http://www.fgdsb.com/2015/01/03/subtree/
+ * @author kkdpan
+ *
+ */
+public class Subtree {
+	
+	public static boolean isSubtree(TreeNode t1, TreeNode t2) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(t1);
+		while (!stack.isEmpty()) {
+			TreeNode n = stack.pop();
+			if (n.val == t2.val && isSameTree(n, t2)) {
+				return true;
+			}
+			stack.push(n.right);
+			stack.push(n.left);
+		}
+		return false;
+	}
+	
+	private static boolean isSameTree(TreeNode t1, TreeNode t2) {
+		if (t1 == null || t2 == null) {
+			return t1 == t2;
+		}
+		if (t1.val != t2.val) {
+			return false;
+		} else {
+			return isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
+		}
+	}
+}
