0110b6a298fb163fb4e3b481945be345e6997ca5
Di Pan <kkdpan@gmail.com>
2015-11-20 17:39:27 -0800

    Add MinSumMahattanDistance.

---
+++ src/MiscQuestions/MinSumMahattanDistance.java
@@ -0,0 +1,55 @@
+package MiscQuestions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MinSumMahattanDistance {
+	
+	private static class Point {
+		private int x;
+		private int y;
+		
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	public static int minDist(Point[] points) {
+		int l = points.length;
+		Arrays.sort(points, (p1, p2) -> p1.x - p2.x);
+		Point px = find(points, l / 2, (p1, p2) -> p1.x - p2.x);
+		Arrays.sort(points, (p1, p2) -> p1.y - p2.y);
+		Point py = find(points, 1 / 2, (p1, p2) -> p1.y - p2.y);
+		int dist = 0;
+		for (Point p : points) {
+			dist += Math.abs(px.x - p.x) + Math.abs(py.y - p.y);
+		}
+		return dist;
+	}
+	
+	private static Point find(Point[] points, int rank, Comparator<Point> cmp) {
+		return findHelper(points, rank, 0, points.length - 1, cmp);
+		
+	}
+	
+	private static Point findHelper(Point[] points, int rank, int b, int e, Comparator<Point> cmp) {
+		if (b < e) {
+			int randomSeed = (int)(Math.random() * (b - e) + b);
+			int pos = partition(points, randomSeed, b, e, cmp);
+			if (pos == rank) {
+				return points[pos];
+			} else if (pos > rank) {
+				return findHelper(points, rank, b, pos - 1, cmp);
+			} else {
+				return findHelper(points, rank, pos + 1, e, cmp);
+			}
+		} else {
+			return points[b];
+		}
+	}
+	
+	private static int partition(Point[] points, int pvt, int b, int e, Comparator<Point> cmp) {
+		throw new RuntimeException();
+	}
+}
