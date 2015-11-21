35ad38d9bc7ebc5bfcb7cd41a2d6a9ff96271fbc
pdkk2004 <kkdpan@gmail.com>
2015-11-20 23:19:00 -0800

    Add MaxBinaryGap. http://www.fgdsb.com/2015/01/03/max-binary-gap/

---
+++ src/MiscQuestions/MaxBinaryGap.java
@@ -0,0 +1,40 @@
+package MiscQuestions;
+
+import org.junit.Test;
+
+import org.junit.*;
+
+/**
+ * http://www.fgdsb.com/2015/01/03/max-binary-gap/
+ * @author kkdpan
+ *
+ */
+public class MaxBinaryGap {
+
+	public static int maxBineryGap(int num) {
+		int max = 0;
+		int c = -1;
+		while (num != 0) {
+			if (c < 0 && (num & 1) == 1) {
+				c = 0;
+				num >>>= 1;
+				continue;
+			}
+			if (c >= 0) {
+				if ((num & 1) == 0) {
+					c++;
+				} else {
+					max = Math.max(c, max);
+					c = 0;
+				}
+			}
+			num >>>= 1;
+		}
+		return max;
+	}
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(2, maxBineryGap(9));
+	}
+}
