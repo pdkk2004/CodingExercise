f4f79fe88eff55a322c19daa3cbb915e38b7b018
Di Pan <kkdpan@gmail.com>
2015-11-30 11:45:39 -0800

    Merge TreeNodeWithParent

--- src/Common/TreeNodeWithParent.java
+++ src/Common/TreeNodeWithParent.java
@@ -7,4 +7,9 @@ public class TreeNodeWithParent extends TreeNode {
 	public TreeNodeWithParent(int v) {
 		super(v);
 	}
+	
+	public TreeNodeWithParent(int v, TreeNodeWithParent p) {
+		this(v);
+		this.p = p;
+	}
 }
