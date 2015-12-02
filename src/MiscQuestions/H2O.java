4fc727b437cb701126e3dac599cd39ec3504ee1f
Di Pan <kkdpan@gmail.com>
2015-12-02 11:33:31 -0800

    Fix bug in H2O solution and test

--- src/MiscQuestions/H2O.java
+++ src/MiscQuestions/H2O.java
@@ -4,47 +4,55 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class H2O {
-	
+
 	private int quotaH = 2;
 	private int quotaO = 1;
 	private ReentrantLock lock;
 	private Condition conditionH;
 	private Condition conditionO;
-	
+
 	public H2O() {
 		this.lock = new ReentrantLock();
 		conditionH = lock.newCondition();
 		conditionO = lock.newCondition();
 	}
-	
+
 	public void O() throws InterruptedException {
 		lock.lock();
-		while (quotaO < 1) {
-			conditionO.wait();
+		try {
+			while (quotaO < 1) {
+				conditionO.await();
+			}
+			System.out.print("O");
+			if (--quotaO == 0 && quotaH == 0) {
+				quotaH = 2;
+				quotaO = 1;
+				System.out.println(" water");
+				conditionH.signalAll();
+				conditionO.signalAll();
+			}
+		} finally {
+			lock.unlock();
 		}
-		System.out.print("O");
-		if (--quotaO == 0 && quotaH == 0) {
-			quotaH = 2;
-			quotaO = 1;
-			System.out.println("water");
-		}
-		conditionO.notifyAll();
-		lock.unlock();
 	}
-	
+
 	public void H() throws InterruptedException {
 		lock.lock();
-		while (quotaH < 1) {
-			conditionH.wait();
+		try {
+			while (quotaH < 1) {
+				conditionH.await();
+			}
+			System.out.print("H");
+			if (--quotaH == 0 && quotaO == 0) {
+				quotaH = 2;
+				quotaO = 1;
+				System.out.println(" water");
+				conditionO.signalAll();
+				conditionH.signalAll();
+			}
+		} finally {
+			lock.unlock();
 		}
-		System.out.print("H");
-		if (--quotaH == 0 && quotaO == 0) {
-			quotaH = 2;
-			quotaO = 1;
-			System.out.println("water");
-		}
-		conditionH.notifyAll();
-		lock.unlock();
 	}
-	
+
 }
--- test/test/miscQuestions/H2OTest.java
+++ test/test/miscQuestions/H2OTest.java
@@ -14,20 +14,22 @@ public class H2OTest {
 	@Test
 	public void testH2O() {
 		H2O h2o = new H2O();
-		for (int i = 0; i < 30; i++) {
-			Selection s = select();
+		Selection[] input = {Selection.O, Selection.H, Selection.H, Selection.H, Selection.H, Selection.H, Selection.O, Selection.H, Selection.H};
+		for (Selection s : input) {
 			Runnable r = null;
 			switch(s) {
 			case O:
 				r = () -> {try {
 					h2o.O();
 				} catch (Exception e) {
+					e.printStackTrace();
 				}};
 				break;
 			case H:
 				r = () -> {try {
 					h2o.H();
 				} catch (Exception e) {
+					e.printStackTrace();
 				}};
 				break;
 			}
@@ -44,6 +46,7 @@ public class H2OTest {
 			try {
 				h2o.O();
 			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
 		};
 		Thread t2 = new Thread(r2);
@@ -53,17 +56,17 @@ public class H2OTest {
 			try {
 				h2o.H();
 			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
 		};
 		Thread t1 = new Thread(r1);
 		t1.start();
-
-
 		
 		Runnable r3 = () -> {
 			try {
 				h2o.H();
 			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
 		};
 		
@@ -71,13 +74,28 @@ public class H2OTest {
 		t3.start();
 	}
 	
-	private Selection select() {
-		int num = (int)(Math.random() * 3);
-		if (num == 0) {
-			return Selection.O;
-		} else {
-			return Selection.H;
-		}
+	@Test
+	public void testH2OTwoO() {
+		H2O h2o = new H2O();
+
+		Runnable r1 = () -> {
+			try {
+				h2o.O();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		};
+		Thread t1 = new Thread(r1);
+		t1.start();
+		
+		Runnable r2 = () -> {
+			try {
+				h2o.O();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		};
+		Thread t2 = new Thread(r2);
+		t2.start();
 	}
-	
 }
