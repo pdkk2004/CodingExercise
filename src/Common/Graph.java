ce3029658a705886b5925c244f36c49d1788e928
Di Pan <kkdpan@gmail.com>
2015-12-06 21:43:53 -0800

    modify graph.

--- src/Common/Graph.java
+++ src/Common/Graph.java
@@ -1,17 +1,40 @@
 package Common;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Graph<T> {
 
-	private Set<GraphNode<T>> roots;
+	private Map<T, GraphNode<T>> verticesMap;
 	
 	public Graph() {
-		this.roots = new HashSet<GraphNode<T>>();
+		this.verticesMap = new HashMap<T, GraphNode<T>>();
 	}
 	
-	public Set<GraphNode<T>> getRoots() {
-		return this.roots;
+	public Set<GraphNode<T>> getVertices() {
+		return new HashSet<>(this.verticesMap.values());
+	}
+	
+	public void addEdge(T to, T from) {
+		verticesMap.putIfAbsent(to, new GraphNode<>(to));
+		verticesMap.putIfAbsent(from, new GraphNode<>(from));
+		verticesMap.get(from).addChild(verticesMap.get(to));
+	}
+	
+	public void addVertex(T t) {
+		if (verticesMap.containsKey(t)) {
+			throw new IllegalArgumentException("Value to be add already exist in graph.");
+		}
+		verticesMap.put(t, new GraphNode<>(t));
+	}
+	
+	public int numOfVertices() {
+		return verticesMap.size();
+	}
+	
+	public boolean contains(T t) {
+		return verticesMap.containsKey(t);
 	}
 }
