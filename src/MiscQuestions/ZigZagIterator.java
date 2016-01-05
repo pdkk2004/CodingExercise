003190b5fe00b522df9e1cee634b934c0a45d6e6
pdkk2004 <kkdpan@gmail.com>
2016-01-04 22:26:30 -0800

    Clean up ZigZagIterator.java

--- src/MiscQuestions/ZigZagIterator.java
+++ src/MiscQuestions/ZigZagIterator.java
@@ -55,20 +55,6 @@ public class ZigZagIterator<E> {
 			sb.append(c);
 		}
 		return sb.toString();
-		
-//		ArrayList<Iterator<Character>> iters = new ArrayList<>();
-//		iters.add(it1);
-//		iters.add(it2);
-//		int count = 0;
-//		StringBuilder sb = new StringBuilder();
-//		while (iters.get(0).hasNext() && iters.get(1).hasNext()) {
-//			sb.append(iters.get(count % 2).next());
-//			count++;
-//		}
-//		
-//		it1.forEachRemaining(c -> sb.append(c));
-//		it2.forEachRemaining(c -> sb.append(c));
-//		return sb.toString();
 	}
 	
 	public static void main(String[] args) {
