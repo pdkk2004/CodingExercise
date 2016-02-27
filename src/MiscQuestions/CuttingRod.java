cd16db8d405d1d1f2cc76ab863f1a6bf2fdb84a7
pdkk2004 <kkdpan@gmail.com>
2016-02-26 21:51:46 -0800

    Refactor CuttingRod solution

--- src/MiscQuestions/CuttingRod.java
+++ src/MiscQuestions/CuttingRod.java
@@ -17,8 +17,7 @@ public class CuttingRod {
 			return 0;
 		}
 		
-		P[1] = prices[0];
-		for (int i = 2; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			int max = prices[i - 1];  //Note: Initialize max to be prices[i - 1], which means no cut.
 			for (int j = 1; j < i; j++) {
 				max = Math.max(max, P[i - j] + prices[j - 1]);
